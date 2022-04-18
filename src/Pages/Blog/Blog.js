48d50ca63ec5b53f23d2e0e22dcdb6a86d8aa1ca
sajolbhadra <sajolbhadra@gmail.com>
2022-04-18 18:37:30 +0600

    qa/blog  added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = () => {
     return (
         <div>
             <h2>Blog</h2>
+            <div className='qa-container'>
+                <div className='qa'>
+                    <h3>Difference between authorization and authentication</h3>
+                    <p>React is a Javascript library. When we made a react app, React creates a virtual dom internally by combining all the components and send it to the browser.
+                        The browser renders it and displays it. If the user does something in UI and needs to update the Browser dom, React compares only the changed part with its old virtual dom and updates it significantly faster-using diff algorithm and sends the browser only the changed part for the update. The browser immediately updates it in browser dom and displays it.
+                    </p>
+                </div>
+                <div className='qa'>
+                    <h3>Why are you using firebase? What other options do you have to implement authentication?</h3>
+                    <p>Props is the short form of Property. It is used to transfer data from parent component to its child component.
+                        To pass data from parent to child first have to create a callback function and into child component pass the function as a prop to the child component. In child Component using destructuring data can be used.
+                        The props is unchangeable.  As the data coming from the parent component it can not be changed by child component.
+                    </p>
+                </div>
+                <div className='qa'>
+                    <h3>What other services does firebase provide other than authentication</h3>
+                    <p>
+                        useState is the built in hook in React. useState used to set any state into it. It set an initial value and returns the current value of the state. For the data that frequently update need to use useState.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
