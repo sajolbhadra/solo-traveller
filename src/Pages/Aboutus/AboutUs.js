a69106ac3c513e0b3fd49dbb8c5905ef9a9a8b1b
sajolbhadra <sajolbhadra@gmail.com>
2022-04-18 18:49:07 +0600

    about me updated

--- src/Pages/Aboutus/AboutUs.js
+++ src/Pages/Aboutus/AboutUs.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import me from '../../img/sajol.jpg'
+import './AboutUs.css'
 
 const AboutUs = () => {
     return (
         <div>
-            <h1>About Us</h1>
+            <h1 className='text-center'>About Me</h1>
+            <img src={me} alt='sajol bhadra' className='d-flex mx-auto image rounded-circle'></img>
+            <h3 className='text-center'>Sajol Bhadra</h3>
+            <h3 className='text-center'>I am a student in BS.c in Electrical and Electronics Engineerning in North Western University, Khulna.</h3>
         </div>
     );
 };
