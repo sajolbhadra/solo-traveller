b7d63f86670073be6b24d30e38e1222b87db72ff
sajolbhadra <sajolbhadra@gmail.com>
2022-04-18 14:22:56 +0600

    header footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import  './Footer.css'
 
 const Footer = () => {
     return (
         <div>
-            <h1>This is footer</h1>
+            <footer className="footer">
+               <div className="container">
+                   <div className="row">
+                        <div className="footer-col">
+                            <h4>About BD Tours and Travel</h4>
+                            <ul>
+                                <li><a href="https://www.google.com">About Us</a> </li>
+                                <li><a href="https://www.google.com">Our Story</a> </li>
+                                <li><a href="https://www.google.com">Our Services</a> </li>
+                            </ul>
+                        </div>
+
+                        <div className="footer-col">
+                            <h4>Follow Us Online</h4>
+                            <div className="social-links">
+                               <a href="https://www.google.com"><i className="fab fa-facebook-f"></i></a> 
+                               <a href="https://www.google.com"><i className="fab fa-twitter"></i></a> 
+                               <a href="https://www.google.com"><i className="fab fa-instagram"></i></a> 
+                               <a href="https://www.google.com"><i className="fab fa-linkedin-in"></i></a> 
+                                
+                            </div>
+                        </div>
+                   </div>
+               </div>
+               <h6 style={{color:'white', marginTop:'35px'}} className="text-center"> All © copyright reserved 2020-2021</h6>
+           </footer>
         </div>
     );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <>
+            <Navbar bg="primary" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            
+        </>
     );
 };
 
